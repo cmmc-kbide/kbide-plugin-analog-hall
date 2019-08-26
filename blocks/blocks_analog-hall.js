407bbba3b1905ae95317642a61d6c5273e4de6a1
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 11:48:58 +0700

    add generator

--- blocks/blocks_analog-hall.js
+++ blocks/blocks_analog-hall.js
@@ -1,15 +1,18 @@
-Blockly.Blocks['ky_003_analog_hall_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-003 ANALOG HALL");
-    this.appendValueInput("KY_003_ANALOG_HALL_PIN")
-        .setCheck(null)
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_003_analog_hall_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-003 ANALOG HALL");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_003_ANALOG_HALL_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_003_ANALOG_HALL_VALUE"),
+          "KY_003_ANALOG_HALL_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_analog-hall.js
+++ blocks/generators_analog-hall.js
@@ -1,6 +1,23 @@
-Blockly.JavaScript['ky_003_analog_hall_block'] = function(block) {
-  var value_ky_003_analog_hall_pin = Blockly.JavaScript.valueToCode(block, 'KY_003_ANALOG_HALL_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_003_analog_hall_block"] = function(block) {
+    var text_ky_003_analog_hall_pin = block.getFieldValue(
+      "KY_003_ANALOG_HALL_PIN"
+    );
+    var variable_ky_003_analog_hall_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_003_ANALOG_HALL_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	
+	#VARIABLE
+	int KY_003_ANALOG_HALL_PIN = ${text_ky_003_analog_hall_pin}; //sensor pin
+	#END
+
+	#SETUP
+		pinMode(KY_003_ANALOG_HALL_PIN, INPUT); //set sensor pin as input
+	#END
+
+	${variable_ky_003_analog_hall_value} = digitalRead(KY_003_ANALOG_HALL_PIN); //Read the sensor
+  `;
+    return code;
+  };
